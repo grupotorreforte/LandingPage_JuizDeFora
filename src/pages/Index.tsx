98164fdb02c555eada9a9dd6f48f746576513591
Joao Marcos dos Santos Valente <joaomvalente@MacBook-Air-de-Joao-Marcos.local>
2026-01-07 13:46:10 -0300

    Feat: Primeiro feat

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ import AudioPlayer from "@/components/AudioPlayer";
 import SocialLinks from "@/components/SocialLinks";
 import bgDesktop from "@/assets/bg-desktop.png";
 import bgMobile from "@/assets/bg-mobile.png";
-import logoSite from "@/assets/logoSite.svg";
+import logoSitee from "@/assets/logoSitee.svg";
 import { Link } from "react-router-dom";
 import { Video } from "lucide-react";
 const Index = () => {
@@ -30,7 +30,7 @@ const Index = () => {
           
         <div className="flex justify-center mb-10 animate-fade-in">
           <img
-            src={logoSite}
+            src={logoSitee}
             alt="Logo Rádio"
             className="w-50 md:w-100"
           />
@@ -51,7 +51,8 @@ const Index = () => {
 
       <section className="relative -mt-8 md:-mt-10 px-4 z-20 animate-scale-in">
         <AudioPlayer />
-        <div className="flex justify-center mt-6">
+        
+        {/* <div className="flex justify-center mt-6">
           <Link 
             to="/video"
             className="flex items-center gap-2 bg-primary hover:bg-primary/90 text-primary-foreground px-6 py-3 rounded-full font-semibold shadow-lg transition-all duration-300 hover:scale-105"
@@ -59,13 +60,13 @@ const Index = () => {
             <Video size={20} />
             Assistir ao Vivo
           </Link>
-        </div>
+        </div> */}
       </section>
 
       <section className="bg-background py-12 md:py-16 px-4 flex-1">
         <div className="container mx-auto max-w-3xl space-y-6 text-center">
           <p className="text-foreground text-base md:text-lg leading-relaxed">
-            A Rádio 89.7 Maravilha FM é a sua companhia diária, levando até você muito mais do que música: transmitimos fé, esperança e a Palavra de Deus, sempre com uma programação de excelência feita para abençoar e transformar vidas.
+            A Rádio 94.7 Maravilha FM é a sua companhia diária, levando até você muito mais do que música: transmitimos fé, esperança e a Palavra de Deus, sempre com uma programação de excelência feita para abençoar e transformar vidas.
           </p>
 
           <p className="text-foreground text-base md:text-lg leading-relaxed">
@@ -73,7 +74,7 @@ const Index = () => {
           </p>
 
           <p className="text-foreground text-base md:text-lg leading-relaxed">
-            Em Uberlândia, sintonize 89.7 FM e viva a experiência de ter a Maravilha FM como parte da sua rotina. Porque aqui, cada canção e cada palavra são preparadas especialmente para você!
+            Em João Pinheiro, sintonize 94.7 FM e viva a experiência de ter a Maravilha FM como parte da sua rotina. Porque aqui, cada canção e cada palavra são preparadas especialmente para você!
           </p>
         </div>
       </section>
@@ -86,7 +87,7 @@ const Index = () => {
           {/* Logo */}
           <div className="flex justify-center md:justify-start w-full md:w-auto">
             <img
-              src={logoSite}
+              src={logoSitee}
               alt="Logo Rádio Maravilha"
               className="w-20 animate-fade-in"
             />
